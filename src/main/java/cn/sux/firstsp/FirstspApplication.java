292d6f2e3bd70a9871762116c6bb69022635df9f
Sux79 <1021773496@qq.com>
2019-08-27 17:34:50 +0800

    创建springboot

---
+++ src/main/java/cn/sux/firstsp/FirstspApplication.java
@@ -0,0 +1,13 @@
+package cn.sux.firstsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/sux/firstsp/FirstspApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.sux.firstsp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstspApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
