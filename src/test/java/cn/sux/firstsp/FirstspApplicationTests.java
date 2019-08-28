5bd059c88e2a45a235c7903f49e970258f8071c4
Sux79 <1021773496@qq.com>
2019-08-28 17:31:43 +0800

    创建sql包

--- src/test/java/cn/sux/firstsp/FirstspApplicationTests.java
+++ src/test/java/cn/sux/firstsp/FirstspApplicationTests.java
@@ -1,5 +1,6 @@
 package cn.sux.firstsp;
 
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,18 @@ public class FirstspApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void pass(){
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        textEncryptor.setPassword("");
+        String url =textEncryptor.encrypt("");
+        String name = textEncryptor.encrypt("");
+        String pasw = textEncryptor.encrypt("");
+        System.out.println("url:"+url);
+        System.out.println("name:"+name);
+        System.out.println("pasw:"+pasw);
     }
 
 }
